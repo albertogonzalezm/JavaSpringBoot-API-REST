d9aece7e9ce8d3106ae383ec353b9f4105d16068
albertogonzalezm <albertogonzalezmantilla@gmail.com>
2023-10-18 17:50:04 -0500

    add swagger-ui

--- src/main/java/com/example/apirest/controller/ClientController.java
+++ src/main/java/com/example/apirest/controller/ClientController.java
@@ -14,14 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -174,7 +167,7 @@ public class ClientController {
                             .message("Client list is Empty")
                             .list(clients)
                             .build(),
-                    HttpStatus.NOT_FOUND);
+                    HttpStatus.OK);
         }
 
         List<ClientDto> clientsDto = clients.stream().map(client -> ClientDto.builder()
--- src/main/java/com/example/apirest/model/entity/Client.java
+++ src/main/java/com/example/apirest/model/entity/Client.java
@@ -4,19 +4,10 @@
  */
 package com.example.apirest.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 /**
  *
